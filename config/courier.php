f3158692dbc7f1506ff77a9d16b0b5e3b714ed41
Oaparecido <daniel.aparecido4683@gmail.com>
2021-05-25 15:24:51 -0300

    🔧 update files config and remove namespace

--- config/courier.php
+++ config/courier.php
@@ -2,7 +2,15 @@
 
 return [
 
-    'mailer' => env('COURIER_MAILER', 'mailtrap'),
+    'host' => env('COURIER_MAILER', 'smtp.mailtrap.io'),
     'email_sender' => env('COURIER_EMAIL_SENDER', 'no-reply-to@default.com'),
-    'name_sender' => env('COURIER_NAME_SENDER', 'Robot')
+    'name_sender' => env('COURIER_NAME_SENDER', 'Robot'),
+    'exceptions' => env('COURIER_EXCEPTIONS', false),
+
+    'smtp' => [
+        'auth' => env('COURIER_SMTP_AUTH', true),
+        'port' => env('COURIER_SMTP_PORT', '2525'),
+        'username' => env('COURIER_SMTP_USERNAME',  '8b9256a7d74c85'),
+        'password' => env('COURIER_SMTP_PASSWORD', '986bf3e32695cd'),
+    ]
 ];
