513d6e17ebbe1890d2aaec86166b57aeb5c03e55
Oaparecido <daniel.aparecido4683@gmail.com>
2021-05-25 15:33:44 -0300

    ✅ update test

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,18 +1,14 @@
 <?php
 
 use App\Services\UserWelcomeMail;
-use App\Services\Templates\ApprovedTemplate;
 use Oaparecido\Courier\Courier;
 
 test('Courier package init', function () {
 
-    /**
-     * Esperar um array ou uma string
-     * se for um array faça um loop
-     * 
-     * Deixar a responsabilidade somente em uma classe para email.
-     */
-    Courier::dispatcher((new UserWelcomeMail()), 'Daniel Aparecido|daniel.aparecido@gmail.com');
+    $success = Courier::dispatcher((new UserWelcomeMail()), 'Daniel Aparecido|daniel.aparecido@gmail.com');
 
-    expect([])->toBeArray();
+    expect($success)->toBe([
+        'status' => true,
+        'message' => 'e-mail enviado com sucesso'
+    ]);
 });
