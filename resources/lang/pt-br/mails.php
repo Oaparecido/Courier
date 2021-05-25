e2584381801cd3040f7ceaa54e94e457435b9747
Oaparecido <daniel.aparecido4683@gmail.com>
2021-05-25 14:14:36 -0300

    ✨ introduce files to translate

---
+++ resources/lang/pt-br/mails.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+
+    'approved' => [
+        'greeting' => 'Olá novo mundo!',
+        'content' => 'O seu novo mundo acabou de ser criado, venha fazer parte
+                      dessa aventura!'
+    ]
+];
