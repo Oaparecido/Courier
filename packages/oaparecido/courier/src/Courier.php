5466cc79398092fdaad5397c741fccd6650f6ad2
Oaparecido <daniel.aparecido4683@gmail.com>
2021-05-25 14:07:38 -0300

    ♻️  refactor code to get configs

--- packages/oaparecido/courier/src/Courier.php
+++ packages/oaparecido/courier/src/Courier.php
@@ -3,49 +3,64 @@
 
 namespace Oaparecido\Courier;
 
-use Oaparecido\Courier\Manipulators\MailHandler;
-use Oaparecido\Courier\Services\TemplateService;
+use Oaparecido\Courier\Manipulators\MailService;
 use PHPMailer\PHPMailer\PHPMailer;
 
-abstract class Courier
+class Courier
 {
-    /**
-     * 1 começar a dar corpo:
-     * [] melhorar nomes;
-     * [] separar responsabilidades das classes;
-     *      classes:
-     *      - configurations
-     *      - templates
-     * [] implamentar configurações para envio com SES
-     */
-
-    final public static function dispatcher(MailHandler $handler, TemplateService $manipulator): array
+    public static function dispatcher(MailService $mailService, array|string $configSender): array
     {
-        // isso é LARAVEL
-        // isso pode ser usado para:
-        //  -> para todo tipo de configuração que possa variar dependendo do projeto.
-        //      ex: Configurarções do amazonSES.
-        // dd(config('hermesMail.name'));
-
-        $manipulator->init();
+        $config_sender = self::transform($configSender);
 
         $mail = new PHPMailer();
         $mail->isSMTP();
-        $mail->Host = 'smtp.mailtrap.io';
-        $mail->SMTPAuth = true;
-        $mail->Port = 2525;
-        $mail->Username = '8b9256a7d74c85';
-        $mail->Password = '986bf3e32695cd';
-        $mail->setFrom($handler->sender, $handler->fromName);
-        $mail->addReplyTo($handler->sender, $handler->fromName);
-        $mail->addAddress('daniel.aparecido@maquinadobem.com', 'Daniel Aparecido');
-        $mail->Subject = $handler->getSubject();
         $mail->CharSet = 'UTF-8';
-        $mail->msgHTML($manipulator->getHtml());
+        $mail->setFrom(config('courier.email_sender'), config('courier.name_sender'));
+        $mail->addAddress($config_sender['to_email'], $config_sender['to_name']);
+        $mail->Subject = $mailService->subject;
+        $mail->Body = $mailService->html;
+        $mail->isHTML(true);
+
+        switch (config('courier.mailer')) {
+            case 'amazon_ses':
+                self::senderSES($mail);
+                break;
+
+            default:
+                self::senderDefault($mail);
+                break;
+        }
 
         if (!$mail->send())
             return ['status' => false, 'error' => $mail->ErrorInfo];
 
         return ['status' => true, 'message' => 'e-mail enviado com sucesso'];
+    }
+
+    private static function senderSES(PHPMailer $mail): void
+    {
+    }
+
+    private static function senderDefault(PHPMailer $mail): void
+    {
+        $mail->Host = 'smtp.mailtrap.io';
+        $mail->SMTPAuth = true;
+        $mail->Port = 2525;
+        $mail->Username = '8b9256a7d74c85';
+        $mail->Password = '986bf3e32695cd';
+    }
+
+    private static function transform(array|string $configSender): array
+    {
+        if (!is_array($configSender)) {
+            $config_sender = explode('|', $configSender);
+            // TODO: Verify if $config_sender[1] is e-mail 
+            $config_sender = [
+                'to_name' => $config_sender[0],
+                'to_email' => $config_sender[1]
+            ];
+        }
+
+        return $config_sender;
     }
 }
