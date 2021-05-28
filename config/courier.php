937fec011f7b11fa411a313406e25694aed2f071
Oaparecido <daniel.aparecido4683@gmail.com>
2021-05-28 11:01:12 -0300

    ♻️  refactor configs, for get by mailer driver

--- config/courier.php
+++ config/courier.php
@@ -1,16 +1,25 @@
 <?php
 
 return [
-    'locale' => env('COURIER_LOCALE', 'pt-br'),
-    'host' => env('COURIER_MAILER', 'smtp.mailtrap.io'),
-    'email_sender' => env('COURIER_EMAIL_SENDER', 'no-reply-to@default.com'),
-    'name_sender' => env('COURIER_NAME_SENDER', 'Robot'),
-    'exceptions' => env('COURIER_EXCEPTIONS', false),
 
-    'smtp' => [
-        'auth' => env('COURIER_SMTP_AUTH', true),
-        'port' => env('COURIER_SMTP_PORT', '2525'),
-        'username' => env('COURIER_SMTP_USERNAME',  '8b9256a7d74c85'),
-        'password' => env('COURIER_SMTP_PASSWORD', '986bf3e32695cd'),
-    ]
+    'default' => env('COURIER_MAILER', 'smtp'),
+
+    'mailers' => [
+
+        'smtp' => [
+            'auth' => env('COURIER_SMTP_AUTH', true),
+            'port' => env('COURIER_SMTP_PORT', '2525'),
+            'username' => env('COURIER_SMTP_USERNAME',  '8b9256a7d74c85'),
+            'password' => env('COURIER_SMTP_PASSWORD', '986bf3e32695cd'),
+            'host' => env('COURIER_MAILER', 'smtp.mailtrap.io'),
+        ],
+    ],
+
+    'locale' => env('COURIER_LOCALE', 'pt-br'),
+
+    'email_sender' => env('COURIER_EMAIL_SENDER', 'no-reply-to@default.com'),
+
+    'name_sender' => env('COURIER_NAME_SENDER', 'Robot'),
+
+    'exceptions' => env('COURIER_EXCEPTIONS', false),
 ];
