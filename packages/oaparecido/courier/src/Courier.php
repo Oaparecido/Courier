b54e773b232df9e7c6620f0c9f0173fcd40af9e6
Oaparecido <daniel.aparecido4683@gmail.com>
2021-05-28 11:02:45 -0300

    ✨ add helper to validate and get cofigs mailer

--- packages/oaparecido/courier/src/Courier.php
+++ packages/oaparecido/courier/src/Courier.php
@@ -5,6 +5,7 @@ namespace Oaparecido\Courier;
 
 use Exception;
 use Illuminate\Support\Facades\Validator;
+use Oaparecido\Courier\Services\Configurations;
 use Oaparecido\Courier\Services\MailService;
 use PHPMailer\PHPMailer\PHPMailer;
 
@@ -12,19 +13,15 @@ class Courier
 {
     public static function dispatcher(MailService $mailService, array $receiver): array
     {
-        self::validate($receiver);
+        Configurations::validateReceiver($receiver);
+
         $mailService->start();
 
-        //implementar validação para todos os config
         $mail = new PHPMailer();
-        $mail->isSMTP();
+
+        Configurations::chooseMailer($mail);
+
         $mail->CharSet = 'UTF-8';
-        $mail->Host = config('courier.host');
-        $mail->SMTPAuth = config('courier.smtp.auth');
-        $mail->Port = config('courier.smtp.port');
-        $mail->Username = config('courier.smtp.username');
-        $mail->Password = config('courier.smtp.password');
-        $mail->setFrom(config('courier.email_sender'), config('courier.name_sender'));
         $mail->addAddress($receiver['to_email'], $receiver['to_name']);
         $mail->Subject = $mailService->subject;
         $mail->Body = $mailService->html;
@@ -34,16 +31,5 @@ class Courier
             return ['status' => false, 'messsage' => $mail->ErrorInfo];
 
         return ['status' => true, 'message' => 'e-mail enviado com sucesso'];
-    }
-
-    private static function validate($receiver)
-    {
-        $validator = Validator::make($receiver, [
-            'to_name' => 'required|string|min:3',
-            'to_email' => 'required|email'
-        ]);
-
-        if ($validator->fails())
-            return ['status' => false, 'errors' => $validator->errors()];
     }
 }
