60bb3451e2c007982213ca24edd167bf775ecfc5
Oaparecido <daniel.aparecido4683@gmail.com>
2021-05-31 11:46:57 -0300

    ♻️  Refactor code, add function to validate receiver

--- packages/oaparecido/courier/src/Courier.php
+++ packages/oaparecido/courier/src/Courier.php
@@ -13,23 +13,36 @@ class Courier
 {
     public static function dispatcher(MailService $mailService, array $receiver): array
     {
-        Configurations::validateReceiver($receiver);
+        self::validateReceiver($receiver);
 
         $mailService->start();
 
         $mail = new PHPMailer();
-
-        Configurations::chooseMailer($mail);
-
         $mail->CharSet = 'UTF-8';
         $mail->addAddress($receiver['to_email'], $receiver['to_name']);
         $mail->Subject = $mailService->subject;
         $mail->Body = $mailService->html;
         $mail->isHTML(true);
+        Configurations::setMailer($mail);
 
         if (!$mail->send())
             return ['status' => false, 'messsage' => $mail->ErrorInfo];
 
         return ['status' => true, 'message' => 'e-mail enviado com sucesso'];
+    }
+
+    private static function validateReceiver($receiver)
+    {
+        $rules = [
+            'to_name' => 'required|string|min:3',
+            'to_email' => 'required|email',
+        ];
+
+        $validator = Validator::make($receiver, $rules);
+
+        if ($validator->fails())
+            return ['status' => false, 'errors' => $validator->errors()];
+
+        return $receiver;
     }
 }
