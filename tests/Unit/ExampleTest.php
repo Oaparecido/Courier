c60b2f4bb99324e70e25b5483629c0f11168ab8a
Oaparecido <daniel.aparecido4683@gmail.com>
2021-06-01 15:18:25 -0300

    ✅ update test

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,21 +1,12 @@
 <?php
 
+use App\Services\Courier\UserValidation;
 use App\Services\UserWelcomeMail;
 use Oaparecido\Courier\Courier;
 
 test('Courier package init', function () {
 
-    $name_world = 'Mundo dos sonhos';
-    $name = 'Daniel Aparecido';
-
-    $service_mail = new UserWelcomeMail([
-        'data' => [
-            'name_world' => $name_world
-        ],
-        'greeting' => [
-            'name' => $name
-        ]
-    ]);
+    $service_mail = new UserValidation([]);
 
     $success = Courier::dispatcher($service_mail, [
         'to_name' => 'Daniel Aparecido',
